19cfcdb902a754e11781e061068c934078902e21
rocioibañez <mirta_gayoso@hotmail.com>
2026-02-03 19:44:48 -0300

    Fix CORS origin definitivo

--- src/app.js
+++ src/app.js
@@ -12,24 +12,20 @@ conectarDB();
 
 const app = express();
 app.use(cookieParser());
-// Middleware
 app.use(express.json());
 
-// CORS
+// CORS CORREGIDO
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production"
-      ? "https://plataforma-cursos-frontend-kfm6.onrender.com"
-
-
-      : "http://localhost:5173",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://plataforma-cursos-frontend.onrender.com"
+        : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-
 
 // Rutas principales
 app.use("/api/usuarios", usuarioRoutes);

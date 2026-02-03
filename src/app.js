d1507fd9e1eb577a35663738c976a58feb96cc8c
rocioibañez <mirta_gayoso@hotmail.com>
2026-02-03 20:19:43 -0300

    Fix rutas mis-cursos, logout y CORS

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://plataforma-cursos-frontend.onrender.com"
+        ? "https://plataforma-cursos-frontend-kfm6.onrender.com"
         : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
